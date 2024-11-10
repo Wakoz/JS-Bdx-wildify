0e33e7daf3c3e0781674da114dd002842264f3d9
Ccilpy <cecilepuyol@gmail.com>
2024-11-10 11:15:49 +0100

    updates header with pseudo and searchbar

--- client/src/components/Header/Searchbar/index.tsx
+++ client/src/components/Header/Searchbar/index.tsx
@@ -3,12 +3,10 @@ import "./style.css";
 function Searchbar() {
   return (
     <form action="">
-      <input
-        type="search"
-        placeholder="Recherche..."
-        value=""
-      />
-      <button type="submit" className="search-button">Valider</button>
+      <input type="search" placeholder="Recherche..." value="" />
+      <button type="submit" className="search-button">
+        Valider
+      </button>
     </form>
   );
 }
--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,15 +1,15 @@
 import "./style.css";
-import {useState} from "react";
+import { useState } from "react";
 import Navbar from "./Navbar";
 import Pseudo from "./Pseudo";
 import Searchbar from "./Searchbar";
 
 function Header() {
-    const [showLinks, setShowLinks] = useState(false);
+  const [showLinks, setShowLinks] = useState(false);
 
   return (
     <header>
-      <Navbar showLinks={showLinks} setShowLinks={setShowLinks}/>
+      <Navbar showLinks={showLinks} setShowLinks={setShowLinks} />
       <Pseudo />
       <Searchbar />
     </header>
