72f446179cc42e913ea561df57429391bfbbdf51
Ccilpy <cecilepuyol@gmail.com>
2024-11-13 14:49:36 +0100

    updates header V1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,18 +5,6 @@ function App() {
   return (
     <>
       <Header />
-      <main>
-        <h1>Title Test</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam delectus nisi consectetur. Consequatur pariatur nisi reprehenderit. Laborum velit delectus sequi architecto totam possimus illo tempore! Nobis aspernatur deserunt placeat rem?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam delectus nisi consectetur. Consequatur pariatur nisi reprehenderit. Laborum velit delectus sequi architecto totam possimus illo tempore! Nobis aspernatur deserunt placeat rem?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam delectus nisi consectetur. Consequatur pariatur nisi reprehenderit. Laborum velit delectus sequi architecto totam possimus illo tempore! Nobis aspernatur deserunt placeat rem?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam delectus nisi consectetur. Consequatur pariatur nisi reprehenderit. Laborum velit delectus sequi architecto totam possimus illo tempore! Nobis aspernatur deserunt placeat rem?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam delectus nisi consectetur. Consequatur pariatur nisi reprehenderit. Laborum velit delectus sequi architecto totam possimus illo tempore! Nobis aspernatur deserunt placeat rem?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam delectus nisi consectetur. Consequatur pariatur nisi reprehenderit. Laborum velit delectus sequi architecto totam possimus illo tempore! Nobis aspernatur deserunt placeat rem?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam delectus nisi consectetur. Consequatur pariatur nisi reprehenderit. Laborum velit delectus sequi architecto totam possimus illo tempore! Nobis aspernatur deserunt placeat rem?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam delectus nisi consectetur. Consequatur pariatur nisi reprehenderit. Laborum velit delectus sequi architecto totam possimus illo tempore! Nobis aspernatur deserunt placeat rem?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam delectus nisi consectetur. Consequatur pariatur nisi reprehenderit. Laborum velit delectus sequi architecto totam possimus illo tempore! Nobis aspernatur deserunt placeat rem?</p>
-      </main>
     </>
   );
 }
