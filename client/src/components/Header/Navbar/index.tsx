220ced17d94f2ca3020771180644b20f8ab2a614
Ccilpy <cecilepuyol@gmail.com>
2024-11-15 11:21:45 +0100

    fix menu burger

--- client/src/components/Header/Navbar/index.tsx
+++ client/src/components/Header/Navbar/index.tsx
@@ -6,10 +6,12 @@ interface NavbarProps {
   setShowLinks: (value: boolean) => void;
 }
 
-function Navbar({ showLinks, setShowLinks }: NavbarProps) {
+export default function Navbar({ showLinks, setShowLinks }: NavbarProps) {
   const handleShowLinks = () => {
     setShowLinks(!showLinks);
   };
+  const closeMenu = () => {
+    setShowLinks(false);}
   return (
     <>
       <Link to="/" className="logo-link">
@@ -19,22 +21,22 @@ function Navbar({ showLinks, setShowLinks }: NavbarProps) {
       <nav id="top">
         <ul className={`navbar ${showLinks ? "show-nav" : "hide-nav"}`}>
           <li>
-            <Link to="/artists" className="link">
+            <Link to="/artists" className="link" onClick={closeMenu}>
               Artistes
             </Link>
           </li>
           <li>
-            <Link to="/podcasts" className="link">
+            <Link to="/podcasts" className="link" onClick={closeMenu}>
               Podcasts
             </Link>
           </li>
           <li>
-            <Link to="/concerts" className="link">
+            <Link to="/concerts" className="link" onClick={closeMenu}>
               Concerts
             </Link>
           </li>
           <li>
-            <Link to="/coups-de-coeur" className="link">
+            <Link to="/coups-de-coeur" className="link" onClick={closeMenu}>
               Coups de ❤️
             </Link>
           </li>
@@ -51,5 +53,3 @@ function Navbar({ showLinks, setShowLinks }: NavbarProps) {
     </>
   );
 }
-
-export default Navbar;
