02879c10724b6b5bd9da1795dc156c7505fb5005
H4lfbeard <humbert.thomas.pro@gmail.com>
2024-11-21 10:44:07 +0100

    fix dead code

--- client/src/components/displayBlocks/MyConcertsSearch.tsx
+++ client/src/components/displayBlocks/MyConcertsSearch.tsx
@@ -16,18 +16,6 @@ type ConcertItem = {
 export default function ConcertSearch() {
   const [userConcerts, setUserConcerts] = useState<ConcertItem[]>([]);
 
-  // useEffect(() => {
-  //   fetch(`${import.meta.env.VITE_API_URL}/api/userConcerts`)
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       const shows = data.items.map(
-  //         (item: { show: ConcertItem }) => item.show,
-  //       );
-  //       setUserConcerts(shows);
-  //     })
-  //     .catch((error) => console.error("Error fetching data:", error));
-  // }, []);
-
   useEffect(() => {
     const fetchConcerts = async () => {
       try {
